429a65bf35ed6994142d87beb9687ccb3c82e815
Leon Tur <tutrkov.leon-2@yandex.ru>
2023-01-03 15:49:40 +0300

    Created structure

---
+++ main/resourses.py
@@ -0,0 +1,8 @@
+class Resourse:
+# Класс для ресурсов (Имя, максимальное количество людей, стоимость в очках)
+    def __init__(self, name, border, points):
+        self.name = name
+        self.border = border
+        self.points = points
+
+    
